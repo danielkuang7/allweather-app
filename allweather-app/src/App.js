f3d869b0315c120cbbedd01414fcb97b5db28e65
Daniel Kuang <danielkuang7@github.com>
2020-01-10 21:08:09 -0500

    Added constructor

--- allweather-app/src/App.js
+++ allweather-app/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-
+  constructor(props) {
+    super(props)
+  }
   render(){
     return (
       <div className="App">
