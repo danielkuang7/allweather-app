f6b28ae6c56cfd44b49ad98be6c47a1c9d08ec8f
Daniel Kuang <danielkuang7@github.com>
2020-03-10 20:19:11 -0400

    Created location props

--- allweather-app/src/Info.js
+++ allweather-app/src/Info.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class Info extends Component {
     render () {
         <div className="info"></div>
+    {this.props.location && <div>Location: {this.props.location}</div>}
     }
 }
 
