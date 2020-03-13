5fa8c791a85fb807f1c7598d45fd975207c4efb5
Daniel Kuang <danielkuang7@github.com>
2020-03-13 17:25:24 -0400

    Moved div

--- allweather-app/src/Info.js
+++ allweather-app/src/Info.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 class Info extends Component {
     render () {
-        <div className="info"></div>
+        <div className="info">
     {this.props.location && <div>Location: {this.props.location}</div>}
     {this.props.currentTemperature && <div>Current Temperature: {this.props.currentTemperature} &deg;F </div>}
     {this.props.currentCondition && <div>Current weather condition: {this.props.currentCondition}</div>}
+    </div>
     }
 }
 
