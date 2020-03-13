02f81f108f6283914f99faea0b506a0df76b1e96
Daniel Kuang <danielkuang7@github.com>
2020-03-12 21:03:41 -0400

    Created currentCondition props

--- allweather-app/src/Info.js
+++ allweather-app/src/Info.js
@@ -5,6 +5,7 @@ class Info extends Component {
         <div className="info"></div>
     {this.props.location && <div>Location: {this.props.location}</div>}
     {this.props.currentTemperature && <div>Current Temperature: {this.props.currentTemperature} &deg;F </div>}
+    {this.props.currentCondition && <div>Current weather condition: {this.props.currentCondition}</div>}
     }
 }
 
